73a34723483d11dab1898cea169fdc3a125511a6
Hamudij99 <hamudij8@gmail.com>
2026-02-07 02:37:11 +0100

    save cart in localStorage

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -2,6 +2,8 @@ import { cart, removeFromCart } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 
+let cartSummaryHTML = "";
+
 cart.forEach((cartItem) => {
   const productId = cartItem.productId;
   let matchingProduct;
@@ -12,7 +14,9 @@ cart.forEach((cartItem) => {
   });
 
   cartSummaryHTML += `
-          <div class="cart-item-container">
+          <div class="cart-item-container js-cart-item-container-${
+            matchingProduct.id
+          }">
             <div class="delivery-date">Delivery date: Tuesday, June 21</div>
 
             <div class="cart-item-details-grid">
@@ -93,5 +97,9 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`,
+    );
+    container.remove();
   });
 });

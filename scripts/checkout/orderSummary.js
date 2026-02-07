5b1c092d494769d0c919af75e49b2a295f3704bf
Hamudij99 <hamudij8@gmail.com>
2026-02-07 13:38:45 +0100

    make payment summary interactive

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -7,11 +7,7 @@ import {
   deliveryOptions,
   getDeliveryOption,
 } from "../../data/deliveryOptions.js";
-
-hello();
-const today = dayjs();
-const deliveryDate = today.add(7, "days");
-deliveryDate.format("dddd, MMMM D");
+import { renderPaymentSummary } from "./paymentSummary.js";
 
 export function renderOrderSummary() {
   let cartSummaryHTML = "";
@@ -118,6 +114,7 @@ export function renderOrderSummary() {
         `.js-cart-item-container-${productId}`,
       );
       container.remove();
+      renderPaymentSummary();
     });
   });
 
@@ -126,6 +123,7 @@ export function renderOrderSummary() {
       const { productId, deliveryOptionId } = element.dataset;
       updateDeliveryOption(productId, deliveryOptionId);
       renderOrderSummary();
+      renderPaymentSummary();
     });
   });
 }
